552fe60f50636285d67ba9091abfe339b39d24b7
James Wilkinson <jdwilkinson1994@gmail.com>
2019-07-25 21:41:16 +0800

    added diff function to check for state changes, added check to set State for objects that are an instance of Room Schema, flattened state object

--- server/server/socket/socket.js
+++ server/server/socket/socket.js
@@ -1,71 +1,80 @@
 const { getRoom, updateRoom } = require("./roomDao");
+const diff = require("../helpers/diff");
+const Room = require("../models/Room");
 
-const to = require("../helpers/to");
-const events = ["ADD_TRACK"];
-const dispatch = (room, { type, payload }) => {
+const events = [
+  "ADD_TRACK",
+  "REMOVE_TRACK",
+  "PAUSE",
+  "PLAY",
+  "UPDATE_PLAYLIST"
+];
+const dispatch = (state, { type, payload }) => {
   // Room Reducer
   // Do not use Object to assign or spread as it copies prototype properties left over from schema Object
   // todo add validation
   switch (type) {
     case "ADD_TRACK":
-      const newRoom = room;
-      newRoom.playlist.push(payload.track);
-      return newRoom;
+      const newState = state;
+      newState.playlist.push(payload.track);
+      return newState;
 
     default:
-      return room;
+      return state;
   }
 };
 const setState = (prev, next) => {
-  // All state changes to should invoked with this function
-  // do not use state = newState
+  if (next instanceof Room) {
+    next = JSON.parse(JSON.stringify(next));
+  }
   Object.assign(prev, next);
 };
 
 module.exports = function(io) {
   io.on("connection", function(socket) {
-    const state = { room: null };
+    const state = {};
+
     socket.on("JOIN_ROOM", async room => {
       /**
        * @param {obj} room {_id,name,playlist,subscribers,currentSong}
        */
-      const nextState = { room: await getRoom(room) };
+      const nextState = await getRoom(room);
       if (!nextState) {
         socket.emit("ERROR", `join attempt failed, room not found`);
       } else {
         setState(state, nextState);
-        socket.join(state.room._id);
-        io.in(state.room._id).emit("ROOM_UPDATED", state.room);
+
+        socket.join(state._id);
+        io.in(state._id).emit("ROOM_UPDATED", state);
       }
     });
 
     events.forEach(event => {
       socket.on(event, data => {
         // Update state based on event type
-        const nextState = {
-          room: dispatch(state.room, {
-            type: event,
-            payload: data
-          })
-        };
+        const nextState = dispatch(state, {
+          type: event,
+          payload: data
+        });
         setState(state, nextState);
         // After each update, send updated room to each socket in room
-        io.in(state.room._id).emit("ROOM_UPDATED", state.room);
+        io.in(state._id).emit("ROOM_UPDATED", state);
       });
     });
+
     let prevState = {};
     setInterval(async () => {
       // Every X seconds check if state has changed.
       // If it has, sync socket state with database and update all clients
 
-      // TODO read https://gomakethings.com/getting-the-differences-between-two-objects-with-vanilla-js/
-      // then, create a diff function to test is prevState is different to state
-      const room = await updateRoom(state.room);
-      if (!result) console.log("Error Updating Room");
-      else {
-        setState(state, { room });
-        setState(prevState, state);
-        io.in(state.room._id).emit("ROOM_UPDATED", state.room);
+      if (diff(state, prevState)) {
+        const room = await updateRoom(state);
+        if (!room) console.log("Error Updating Room");
+        else {
+          setState(state, room);
+          setState(prevState, state);
+          io.in(state._id).emit("ROOM_UPDATED", state);
+        }
       }
     }, 3000);
   });
