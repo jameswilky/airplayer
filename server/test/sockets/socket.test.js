0aada97f03a5a3488df1f9edc437f9a7574b32c5
James Wilkinson <jdwilkinson1994@gmail.com>
2019-07-30 21:50:48 +0800

    test appear to be passing more consistently

--- server/test/sockets/socket.test.js
+++ server/test/sockets/socket.test.js
@@ -33,19 +33,6 @@ const wedding = {
   currentSong: { playing: false, trackId: "4as" }
 };
 
-// Set up host server
-beforeEach(done => {
-  [host, url] = Mocket.connectHost();
-  config(host);
-  done();
-});
-
-// Close HTTP server
-afterEach(done => {
-  Mocket.disconnectHost(host);
-  done();
-});
-
 const waitFor = ms => {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
@@ -57,6 +44,12 @@ describe("Sockets backend", () => {
     // Clean Database before each test
     const [err, res] = await to(Room.deleteMany({}));
     if (err) throw "Failed to delete all from Database";
+    [host, url] = Mocket.connectHost();
+    config(host);
+  });
+  afterEach(done => {
+    Mocket.disconnectHost(host);
+    done();
   });
 
   describe("JOIN_ROOM", () => {
@@ -78,7 +71,7 @@ describe("Sockets backend", () => {
         johnState = state;
       });
 
-      await waitFor(10);
+      await waitFor(50);
       expect(johnState.id.toString()).to.eql(room.id.toString()); // TODO use different assertion
       expect(johnState.name).to.eql(birthday.name);
     });
@@ -106,55 +99,6 @@ describe("Sockets backend", () => {
   });
 
   it("should update the state of the room for all users in that room after an action", async () => {
-    // Set up Rooms
-    const birthdayRoom = await createMockRoom(birthday);
-    const weddingRoom = await createMockRoom(wedding);
-
-    const [john, alice, mary] = await Mocket.connectClients(3, url);
-
-    // const alice = await Mocket.connectClient(url);
-    // const mary = await Mocket.connectClient(url);
-    // const john = await Mocket.connectClient(url);
-
-    let johnState;
-    let aliceState;
-    let maryState;
-
-    john.on("ROOM_UPDATED", state => {
-      johnState = state;
-    });
-
-    alice.on("ROOM_UPDATED", state => {
-      aliceState = state;
-    });
-
-    mary.on("ROOM_UPDATED", state => {
-      maryState = state;
-    });
-
-    alice.emit("JOIN_ROOM", birthdayRoom.id);
-    mary.emit("JOIN_ROOM", weddingRoom.id);
-    john.emit("JOIN_ROOM", birthdayRoom.id);
-    // await waitFor(100);
-
-    // let johnError, aliceError, maryError;
-    // john.on("ERROR", msg => (johnError = msg));
-    // alice.on("ERROR", msg => (aliceError = msg));
-    // mary.on("ERROR", msg => (maryError = msg));
-
-    // Add Track and update birthday Room
-    // console.log(maryError, johnError, aliceError);
-
-    // john.emit("ADD_TRACK", { trackId: "test" });
-
-    // Check final states
-    // await waitFor(400);
-    // console.log(msg);
-    // console.log(aliceState, maryState, johnState);
-    // console.log(maryError, johnError, aliceError);
-  });
-
-  it("should send and receive a message only to users in the same room", async () => {
     let john, alice, mary;
     let johnState, aliceState, maryState;
 
@@ -190,7 +134,7 @@ describe("Sockets backend", () => {
       });
     });
 
-    await waitFor(50);
+    await waitFor(100);
     console.log(johnState.playlist);
     console.log(aliceState.playlist);
     console.log(maryState.playlist);
