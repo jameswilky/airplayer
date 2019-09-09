5acbda18c069a0d4eeb400c99f1075a48e26ca4f
James Wilkinson <jdwilkinson1994@gmail.com>
2019-09-09 15:43:29 +0800

    wrote tests for musicSearchInterface

--- client/src/interfaces/MusicSearch/.test.js
+++ client/src/interfaces/MusicSearch/.test.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { shallow } from "enzyme";
 import MusicSearchInterface from "./index";
-import SongList from "../../components/SongList";
-import SearchBar from "../../components/SearchBar";
+import Input from "../../components/Input";
+import { SearchBar } from "./styles";
 
-const setUp = (props = {}) => {
-  const component = shallow(<MusicSearchInterface {...props} />);
-  return component;
-};
+const setUp = (props = {}) => shallow(<MusicSearchInterface {...props} />);
 
 describe("MusicSearchInterface", () => {
   it("should render without errors", () => {
     const component = setUp();
     expect(component.length).toBe(1);
   });
-  it("should render a SongList component", () => {
-    const component = setUp();
-    expect(component.contains(<SongList />)).toBe(true);
-  });
+
   it("should render a SearchBar component", () => {
     const component = setUp();
-    expect(component.contains(<SearchBar />)).toBe(true);
+    const wrapper = component.find(SearchBar);
+    expect(wrapper.length).toBe(1);
+  });
+  it("should render an Input component within a SearchBar component", () => {
+    const component = setUp();
+    const wrapper = component.find(SearchBar).find(Input);
+    expect(wrapper.length).toBe(1);
   });
 });
