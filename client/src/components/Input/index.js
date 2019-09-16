b9d303f29d6d3939c644b42efb8d1861a6df8470
James Wilkinson <jdwilkinson1994@gmail.com>
2019-09-16 14:34:40 +0800

    added test for index component

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Input(props) {
   return (
     <input
+      name={props.name}
       type={props.type}
       value={props.value}
       onChange={e => props.setValue(e.target.value)}
