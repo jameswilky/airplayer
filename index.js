9d3a4d389de72fe12a7318c40d8cd7d0244fb5b8
James Wilkinson <jdwilkinson1994@gmail.com>
2019-12-09 13:59:22 +0800

    changed nodeenvironemnt on start to production form development

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ require("./server/server.js")(app, express, io);
 //     // client side react routes can route relative to this file
 //   });
 // }
-// if (process.env.NODE_ENV === "production") {
-// Express will serve up production assets
-// like our main js file or main.css file
-const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main js file or main.css file
+  const path = require("path");
 
-app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "build")));
 
-// Express will serve up index.html file if it doesnt recognise the route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-  // client side react routes can route relative to this file
-});
-//}
+  // Express will serve up index.html file if it doesnt recognise the route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+    // client side react routes can route relative to this file
+  });
+}
 
 // export our app for testing and flexibility, required by index.js
 const port = process.env.PORT || 8888;
