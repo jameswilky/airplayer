88a83b365d0bc846eb9db4be106491bdfbb92162
James Wilkinson <jdwilkinson1994@gmail.com>
2019-12-09 15:41:42 +0800

    ignoring prod keys

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://james:6o3C7juBTxR8oupd@airplayer-2tifo.mongodb.net/airDb?retryWrites=true&w=majority",
+  SPOTIFY_CLIENT_ID: "3bd98bd2087943aeb9e9dabc459c2a3b",
+  SPOTIFY_CLIENT_SECRET: "1c0c9c0b586d4d0cb05a49a20d01e97c",
+  SPOTIFY_REDIRECT_URI: "http://localhost:8888/auth/callback",
+  FRONTEND_URI: "http://localhost:3000"
+};
