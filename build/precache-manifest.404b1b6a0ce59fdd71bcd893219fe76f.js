b3ec31eda21d01bdb880d0c382c1735607c43ecf
James Wilkinson <jdwilkinson1994@gmail.com>
2019-12-19 18:55:26 +0800

    asd

---
+++ build/precache-manifest.404b1b6a0ce59fdd71bcd893219fe76f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1535ca6462c08907c294a3aa4ee7d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2163bf5ac1189c31011",
+    "url": "/static/css/main.f9fbf225.chunk.css"
+  },
+  {
+    "revision": "52049655a23c43619cfc",
+    "url": "/static/js/2.ab954329.chunk.js"
+  },
+  {
+    "revision": "b2163bf5ac1189c31011",
+    "url": "/static/js/main.0eb8d2c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ddd851ef947ae6dbbeefdb61941263e2",
+    "url": "/static/media/spotifyLogo.ddd851ef.png"
+  }
+]);
